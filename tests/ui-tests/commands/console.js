b1422f3eb52e276c809647148a955a3ca6757c01
Jesus Real <8238188+jesusreal@users.noreply.github.com>
2019-05-10 19:38:15 +0200

    Write test still ugly

--- tests/ui-tests/commands/console.js
+++ tests/ui-tests/commands/console.js
@@ -230,4 +230,5 @@ module.exports = {
   deleteNamespace,
   getApplicationNames,
   getTextContentOnFrameBySelector,
+  getNamesOnCurrentPage
 };
--- tests/ui-tests/tests/console-expose-api.js
+++ tests/ui-tests/tests/console-expose-api.js
@@ -1,3 +1,4 @@
+import request from 'request';
 import config from '../config';
 import kymaConsole from '../commands/console';
 import common from '../commands/common';
@@ -5,18 +6,14 @@ import { describeIf } from '../utils/skip';
 import dex from '../utils/dex';
 import address from '../utils/address';
 import { retry } from '../utils/retry';
-import { testPluggable } from '../setup/test-pluggable';
 import { k8sApiNamespace, k8sApiDeployment, k8sApiService } from "./../setup/k8s-api"
-import { CLOSING } from 'isomorphic-ws';
 
 let page, browser;
 let token = '';
 
-// TODO: Move application tests to a separate file
-const REQUIRED_MODULE = 'application';
 let service, namespace;
 
-describeIf(dex.isStaticUser(), 'Console basic tests', () => {
+describeIf(dex.isStaticUser(), 'Console expose api tests', () => {
   beforeAll(async () => {
     await new k8sApiNamespace();
     service = await new k8sApiDeployment();
@@ -36,15 +33,78 @@ describeIf(dex.isStaticUser(), 'Console basic tests', () => {
     }
   });
 
-  test('Check if namespaces exist', async () => {
+  test('Expose API for Service', async () => {
     const serviceUrl = address.console.getService(config.testNamespace, service.definition.metadata.name);
+    const apiName = "ui-test-exposed-api";
+    let statusCode, frame, exposedApiCellTexts;
+    
     await Promise.all([
       page.goto(serviceUrl),
       page.waitForNavigation({
         waitUntil: ['domcontentloaded', 'networkidle0'],
       }),
     ]);
-    await kymaConsole.openLinkOnFrame(page, "[data-e2e-id=open-expose-api]", "Expose API");    
-    await page.type('#host-input', config.password);
+
+    function callExposedAPI() {
+      const req = {
+        url: `https://${apiName}.${config.domain}`,
+        method: 'GET',
+        headers: { Authorization: token },
+        // TODO: Analyze problem with UNABLE_TO_VERIFY_LEAF_SIGNATURE
+        rejectUnauthorized: false,
+      };
+
+      return new Promise((resolve, reject) => {
+        request(req, (error, response) => {
+          resolve(response);
+        });
+      });
+    }
+
+    frame = await kymaConsole.getFrame(page);
+    await frame.click("[data-e2e-id=open-expose-api]");    
+    await frame.waitForSelector('#host-input');
+    await frame.type('#host-input', apiName);
+    await frame.click("[data-e2e-id=save-expose-api]");    
+
+    await frame.waitFor(4000);
+    statusCode = (await callExposedAPI()).statusCode;
+    expect(statusCode).toEqual(404);
+
+    async function getCellsText() {
+      return {
+        name: (await kymaConsole.getNamesOnCurrentPage(page, "[data-e2e-id=exposed-api-name]"))[0],
+        secured: (await kymaConsole.getNamesOnCurrentPage(page, "[data-e2e-id=exposed-api-secured]"))[0],
+        idp: (await kymaConsole.getNamesOnCurrentPage(page, "[data-e2e-id=exposed-api-idp]"))[0]
+      }
+    }
+    
+    exposedApiCellTexts = await getCellsText();
+    expect(exposedApiCellTexts.name).toEqual(` http-db-service-${apiName} `);
+    expect(exposedApiCellTexts.secured).toEqual(`No`);
+    expect(exposedApiCellTexts.idp).toEqual('-');
+
+
+    await frame.waitFor(4000);
+    statusCode = (await callExposedAPI()).statusCode;
+    expect(statusCode).toEqual(200);
+
+    await kymaConsole.openLinkOnFrame(page, "[data-e2e-id=exposed-api-name]", `http-db-service-${apiName}`);
+    await frame.waitForSelector("[data-e2e-id=secure-api-checkbox]");
+    await frame.click("[data-e2e-id=secure-api-checkbox]");
+    await frame.waitFor(2000);
+    await frame.click("[data-e2e-id=save-expose-api]");    
+
+
+    exposedApiCellTexts = await getCellsText();
+    expect(exposedApiCellTexts.name).toEqual(` http-db-service-${apiName} `);
+    expect(exposedApiCellTexts.secured).toEqual(`Yes`);
+    expect(exposedApiCellTexts.idp).toEqual('DEX');
+
+    await frame.waitFor(4000);
+    statusCode = (await callExposedAPI()).statusCode;
+    expect(statusCode).toEqual(401);
+
+    expect(page.url()).toContain("services");
   });
 });
