aebd3eb563a2ee5b8826b384180059fecdfaf33d
Jesus Real <8238188+jesusreal@users.noreply.github.com>
2019-05-13 20:58:32 +0200

    add waiting periods to make test pass after moving it to console-basic-test file

--- tests/ui-tests/tests/console-basic-test.js
+++ tests/ui-tests/tests/console-basic-test.js
@@ -110,9 +110,11 @@ describeIf(dex.isStaticUser(), 'Console basic tests', () => {
     }
 
     // Create k8s resources
+    await page.waitFor(10000); // wait for namespace from previous test to be removed
     namespace = await new k8sApiNamespace();
     await new k8sApiDeployment();
     service = await new k8sApiService();
+    await page.waitFor(15000); // wait for new resources to be active
 
     serviceUrl = address.console.getService(namespace.definition.metadata.name, service.definition.metadata.name);
     await Promise.all([
