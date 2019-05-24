43e2e9cf9a634f3ac93499e733270b00deb52897
parostatkiem <jan.sudczak@sap.com>
2019-05-24 15:54:18 +0200

    Improve NotificationComponent

--- lambda/src/app/shared/components/notification/notification.component.ts
+++ lambda/src/app/shared/components/notification/notification.component.ts
@@ -6,7 +6,13 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./notification.component.scss'],
 })
 export class NotificationComponent {
-  @Input() type: "info" | "success" | "error" = "info";
+  @Input() type: 'info' | 'success' | 'error' = 'info';
   @Input() header: string;
   @Input() description?: string;
+
+  public iconForType: Map<string, string> = new Map([
+    ['error', 'message-error'],
+    ['info', 'message-information'],
+    ['success', 'message-success'],
+  ]);
 }
