0f6e199bf4c8607c050f89b64773963cd1ec93ac
parostatkiem <jan.sudczak@sap.com>
2019-05-24 15:41:29 +0200

    Add NotificationComponent

---
+++ lambda/src/app/shared/components/notification/notification.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-notification[header]',
+  templateUrl: './notification.component.html',
+  styleUrls: ['./notification.component.scss'],
+})
+export class NotificationComponent {
+  @Input() type: "info" | "success" | "error" = "info";
+  @Input() header: string;
+  @Input() description?: string;
+}
