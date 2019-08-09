af162fc315b31bd8ce19b4dd7366354c8c780b10
y-kkamil <kamil.kupczynski@sap.com>
2019-08-09 15:03:51 +0200

    Add deep linking for instances view

--- components/generic-documentation/src/index.tsx
+++ components/generic-documentation/src/index.tsx
@@ -59,7 +59,7 @@ function renderContent(type: LayoutType, props?: any): React.ReactNode {
       return <CatalogUILayout {...props} renderers={RENDERERS} />;
     }
     case LayoutType.INSTANCES_UI: {
-      return <InstancesUILayout renderers={RENDERERS} />;
+      return <InstancesUILayout {...props} renderers={RENDERERS} />;
     }
     default:
       return null;
