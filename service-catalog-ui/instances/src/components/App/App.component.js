75a73577c8f77231fffc17476c70d94eb8049104
y-kkamil <kamil.kupczynski@sap.com>
2019-08-22 14:08:20 +0200

    Add missing dependencies

--- service-catalog-ui/instances/src/components/App/App.component.js
+++ service-catalog-ui/instances/src/components/App/App.component.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Modal, Notification } from '@kyma-project/react-components';
+import {
+  Modal,
+  Notification,
+  BackendModuleDisabled,
+} from '@kyma-project/react-components';
 
 import DataProvider from '../DataProvider/DataProvider';
 import RouteWrapper from './RouteWrapper';
+
+import { backendModuleExists } from '../../commons/helpers';
 
 Modal.MODAL_APP_REF = '#root';
 const NOTIFICATION_VISIBILITY_TIME = 5000;
