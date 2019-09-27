14f3a4c89f8e2df0b12d1a1fa8259de7ed52e593
y-kkamil <kamil.kupczynski@sap.com>
2019-09-27 14:42:48 +0200

    Adjust dex config

--- compass/src/config/luigi-config/auth.js
+++ compass/src/config/luigi-config/auth.js
@@ -1,12 +1,20 @@
 const domain =
   (window.clusterConfig && window.clusterConfig['domain']) || 'kyma.local';
 
+const authClusterConfig = window.clusterConfig && window.clusterConfig.auth;
+
 const auth = {
   use: 'openIdConnect',
   openIdConnect: {
-    authority: `https://dex.${domain}`,
-    client_id: 'compass-ui',
-    scope: 'audience:server:client_id:compass-ui openid profile email groups',
+    authority: authClusterConfig
+      ? authClusterConfig['authority']
+      : `https://dex.${domain}`,
+    client_id: authClusterConfig
+      ? authClusterConfig['client_id']
+      : 'compass-ui',
+    scope: authClusterConfig
+      ? authClusterConfig['scope']
+      : 'audience:server:client_id:compass-ui openid profile email groups',
     loadUserInfo: false,
     logoutUrl: '/logout.html',
   },
