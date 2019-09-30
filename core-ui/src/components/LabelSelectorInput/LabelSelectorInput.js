f6e9191b33bacd725f4e10dbf96ea3fc67e9bc3d
parostatkiem <jan.sudczak@sap.com>
2019-09-30 10:29:24 +0200

    Display labels in LabelSelectorInput

--- core-ui/src/components/LabelSelectorInput/LabelSelectorInput.js
+++ core-ui/src/components/LabelSelectorInput/LabelSelectorInput.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import './LabelSelectorInput.scss';
 import { Token } from 'fundamental-react/Token';
+import {
+  FormFieldset,
+  FormInput,
+  FormItem,
+  FormLabel,
+  FormLegend,
+  FormMessage,
+  FormRadioGroup,
+  FormRadioItem,
+  FormSelect,
+  FormSet,
+  FormTextarea,
+} from 'fundamental-react/Forms';
 
 //TODO: move this component to a shared "place"
 
 const RemovableLabel = ({ text }) => (
-  <Token onClick={function C() {}}>{text}</Token>
+  <Token className="label" onClick={function C() {}}>
+    {text}
+  </Token>
 );
 
 const LabelSelectorInput = ({
   labels = ['aaaa=bbbbb', 'istio-injection=enabled'],
   readonlyLabels = [],
   onchange,
-}) => (labels.length ? labels.map(l => <RemovableLabel text={l} />) : null);
+}) =>
+  labels.length ? (
+    <div className="fd-form__set">
+      <div className="wrapper">
+        {labels.map(l => (
+          <RemovableLabel text={l} />
+        ))}
+        <input
+          className="fd-form__control"
+          type="text"
+          id="input-1"
+          placeholder="Field placeholder text"
+        />
+      </div>
+    </div>
+  ) : null;
 
 export default LabelSelectorInput;
