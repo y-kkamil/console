9430a589205408872c96386714df67611febf61b
parostatkiem <jan.sudczak@sap.com>
2019-10-03 10:37:21 +0200

    Remove unused import

--- core-ui/src/components/NamespaceList/NamespacesListHeader/NamespacesListHeader.js
+++ core-ui/src/components/NamespaceList/NamespacesListHeader/NamespacesListHeader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Panel, Button, Search } from '@kyma-project/react-components';
+import { Panel, Search } from '@kyma-project/react-components';
 import NamespaceFilters from './NamespaceFilters/NamespaceFilters';
 import './NamespacesListHeader.scss';
 
