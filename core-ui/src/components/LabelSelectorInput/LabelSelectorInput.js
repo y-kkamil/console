5b16cadf879523b265a0fccb2f142a7bd5ad6fc3
Szymon Janota <szymon.janota@sap.com>
2019-10-07 10:02:57 +0200

    Better dot escape

--- core-ui/src/components/LabelSelectorInput/LabelSelectorInput.js
+++ core-ui/src/components/LabelSelectorInput/LabelSelectorInput.js
@@ -7,9 +7,8 @@ import { Token } from 'fundamental-react/Token';
 const domainSegmentRegexp = '([a-z0-9]([a-z0-9-_]{0,61}[a-z0-9])?)';
 
 // Dot needs to be escaped for regexp
-// eslint-disable-next-line no-useless-escape
-const domainRegexp = `(${domainSegmentRegexp}\.)*${domainSegmentRegexp}`;
-const nameAndValueRegexp = '[a-z0-9A-Z]([a-z0-9A-Z-_.]{0,61}[a-z0-9A-Z])?';
+const domainRegexp = `(${domainSegmentRegexp}\\.)*${domainSegmentRegexp}`;
+const nameAndValueRegexp = '[a-z0-9A-Z]([a-z0-9A-Z-_\\.]{0,61}[a-z0-9A-Z])?';
 export const labelRegexp = new RegExp(
   `^((${domainRegexp})/)?${nameAndValueRegexp}=${nameAndValueRegexp}$`,
 );
