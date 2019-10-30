2a02d9c47533e997294ca64ef1b83fa3df59f5f6
y-kkamil <kamil.kupczynski@sap.com>
2019-10-30 15:42:06 +0100

    test

--- compass/src/components/Scenarios/Scenarios.component.js
+++ compass/src/components/Scenarios/Scenarios.component.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { GenericList } from 'react-shared';
+import LuigiClient from '@kyma-project/luigi-client';
 
 import CreateScenarios from './CreateScenario/CreateScenarioModal/CreateScenarioModal.container';
 
