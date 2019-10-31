97904e42755dd5e11829f225b807643cb722a691
y-kkamil <kamil.kupczynski@sap.com>
2019-10-31 15:46:38 +0100

    Add environments to webpack configuration

--- szerdstuff/webpack.config.js
+++ szerdstuff/webpack.config.js
@@ -1,35 +1,37 @@
 const path = require('path');
 
-module.exports = {
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-    library: '',
-    libraryTarget: 'umd',
-  },
-  externals: {
-    react: 'react',
-    'react-dom': 'react-dom',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/react'],
+module.exports = function(env) {
+  return {
+    entry: './index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'index.js',
+      library: '',
+      libraryTarget: 'umd',
+    },
+    externals: {
+      react: 'react',
+      'react-dom': 'react-dom',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/react'],
+          },
         },
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-  performance: {
-    hints: false,
-  },
-  mode: 'production',
+        {
+          test: /\.s[ac]ss$/i,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    performance: {
+      hints: false,
+    },
+    mode: env.production ? 'production' : 'development',
+  };
 };
