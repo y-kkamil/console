389a9bad49a0a1b9f6b931532619347c6dabb7a5
parostatkiem <jan.sudczak@sap.com>
2019-12-17 11:48:23 +0100

    Fix metadataDefinitions test

--- compass/src/components/MetadataDefinitions/MetadataDefinitionDetails/test/MetadataDefinitionDetails.unit.test.js
+++ compass/src/components/MetadataDefinitions/MetadataDefinitionDetails/test/MetadataDefinitionDetails.unit.test.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import { MockedProvider } from '@apollo/react-testing';
 import { mocks } from './mock';
 
 import MetadataDefinitionDetails from '../MetadataDefinitionDetails.container';
 
-const { act } = renderer;
-const wait = require('waait');
-
 describe('MetadataDefinitionDetails', () => {
   it('Renders null schema', async () => {
-    let component;
-    act(() => {
-      component = renderer.create(
-        <MockedProvider mocks={mocks} addTypename={false}>
-          <MetadataDefinitionDetails definitionKey="noschemalabel" />
-        </MockedProvider>,
-      );
-    });
-    await wait(0); // wait for response
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    const { queryByText, queryByLabelText } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <MetadataDefinitionDetails definitionKey="noschemalabel" />
+      </MockedProvider>,
+    );
+
+    await wait(0); // wait  response
+    expect(queryByText('Schema')).toBeInTheDocument();
+    expect(queryByLabelText('schema-editor')).not.toBeInTheDocument();
   });
 });
